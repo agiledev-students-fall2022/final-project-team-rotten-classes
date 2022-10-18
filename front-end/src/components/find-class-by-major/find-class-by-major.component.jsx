484eff80753177fb6f62cc81a24e12cf141c6d8b
luoashley <97135228+luoashley@users.noreply.github.com>
2022-10-17 20:43:00 -0400

    add major

---
+++ front-end/src/components/find-class-by-major/find-class-by-major.component.jsx
@@ -0,0 +1,19 @@
+import './find-class-by-major.styles.scss'
+
+const
+    FindClassByMajor = () => {
+    return (
+        <div className='find-class-by-major-container'>
+            <div className='find-class-by-major-header'>
+                <span className='header-text-span'>Find your classes by major</span>
+                <span className='arrow'>&#8680;</span>
+            </div>
+
+            <div className='find-class-by-major-image-container'>
+
+            </div>
+        </div>
+    )
+};
+
+export default FindClassByMajor;
