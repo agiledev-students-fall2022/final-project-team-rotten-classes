d0414c8089f30fe6b2e6bd241e29a16bd45e5ef5
luoashley <97135228+luoashley@users.noreply.github.com>
2022-10-25 10:06:21 -0400

    Add elements to the search page

--- front-end/src/routes/search-page/search-page.component.jsx
+++ front-end/src/routes/search-page/search-page.component.jsx
@@ -1,13 +1,15 @@
 import SearchBox from "../../components/search-box/search-box.component";
 import './search-page.styles.scss';
+import { Button } from 'react-bootstrap';
+import React from "react";
+
 
 const searchPage = () => {
     return(
         <div>
-            <SearchBox
-                placeholder='Search...'
-                className={'search-box'}
-            />
+            <span ><Button>FILTER</Button></span>
+            <span ><Button>SORT</Button></span>
+            <SearchBox placeholder='Search...' className={'search-box'}/>
         </div>
     )
 }
