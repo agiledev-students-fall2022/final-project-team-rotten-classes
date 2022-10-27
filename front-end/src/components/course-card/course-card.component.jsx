511529adc07e217a2962db62d99841cf9c63830b
luoashley <97135228+luoashley@users.noreply.github.com>
2022-10-27 11:04:20 -0400

    Make the search function searchable. Currently, using mock data(not the actual course data). Will change the data once connected to backend.

--- front-end/src/components/course-card/course-card.component.jsx
+++ front-end/src/components/course-card/course-card.component.jsx
@@ -1,19 +1,14 @@
 import './course-card.styles.css';
 
 const CourseCard = ({course}) => {
-    const {courseID, courseName, courseRate, courseProfessor} = course;
+    const {id, name, email} = course;
     return (
-        <div className='course-card-container' key={courseID}>
-        <img
-            alt={`course ${courseName}`}
+        <div className='card-container' key={id}>
 
-            //Image Source needs to be filled with mock data
-         
-        />
-        <h5>{courseName}</h5>
-        <h6>{courseRate}</h6>
-        <h6>{courseProfessor}</h6>
+            <h2>{name}</h2>
+            <p>{email}</p>
         </div>
-    )};
+    )
+};
 
 export default CourseCard;
--- front-end/src/routes/search-page/search-page.component.jsx
+++ front-end/src/routes/search-page/search-page.component.jsx
@@ -1,21 +1,49 @@
 import SearchBox from "../../components/search-box/search-box.component";
-import './search-page.styles.scss';
+import './search-page.styles.css';
 import React from "react";
 import SortBox from "../../components/sort-box/sort-box.component";
 import FilterBox from "../../components/filter-box/filter-box.component";
+import CourseCardList from "../../components/course-card-list/course-card-list.component";
 
-const searchPage = () => {
+import {useState, useEffect} from "react";
+
+
+const SearchPage = () => {
+    const [searchField, setSearchField] = useState(''); //[value, setValue]
+    const [course, setCourse] = useState([]);
+    const [filteredCourses, setFilterCourses] = useState(course);
+
+    useEffect(() =>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json() )
+            .then((users) => setCourse(users));
+    },[]);
+
+    useEffect(()=>{
+        const newFilteredCourses = course.filter((course) =>{
+            return course.name.toLocaleLowerCase().includes(searchField);
+        });
+        setFilterCourses(newFilteredCourses);
+    },[course, searchField]);
+
+    const onSearchChange = (event) => {
+        const searchFieldString = event.target.value.toLocaleLowerCase();
+        setSearchField(searchFieldString);
+    }
+
+
     return(
         <div className='search-container'>
             <div className>
-                <SearchBox placeholder='Search...' className={'search-box'}/>
+                <SearchBox onChangeHandler={onSearchChange} placeholder='Search...' className={'search-box'}/>
             </div>
             <div className='sort-filter-container'>
                     <SortBox className={'sort-box'}/>
                     <FilterBox className={'filter-box'}/>
             </div>
+            <CourseCardList courses={filteredCourses}/>
         </div>
     )
 }
 
-export default searchPage
+export default SearchPage
