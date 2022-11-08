f097620eeddccb0f87271aa8f0c514e0c35f816b
luoashley <97135228+luoashley@users.noreply.github.com>
2022-11-07 19:28:08 -0500

    push structure

--- back-end/app.js
+++ back-end/app.js
@@ -1,6 +1,25 @@
 // import and instantiate express
 const express = require("express") // CommonJS import style!
 const app = express() // instantiate an Express object
-// we will put some server logic here later...
-// export the express app we created to make it available to other modules
+const bodyParser = require('body-parser');
+const {courseRoutes} = require("./routes/courseRoutes");
+
+// import some useful middleware
+const multer = require("multer") // middleware to handle HTTP POST requests with file uploads
+const axios = require("axios") // middleware for making requests to APIs
+require("dotenv").config({ silent: true }) // load environmental variables from a hidden file named .env
+const morgan = require("morgan") // middleware for nice logging of incoming HTTP requests
+
+// use the morgan middleware to log all incoming http requests
+app.use(morgan("dev")) // morgan has a few logging default styles - dev is a nice concise color-coded style
+app.use(bodyParser.json())
+// app.use('/api/course', courseRoutes => {})
+
+app.use((req, res, next) => {
+    console.log(req.body);
+    next();
+})
+
+
+
 module.exports = app
