d37fa088d5256897b5173b00af8ada68545c795b
4ur1X <ronit.singh@nyu.edu>
2022-11-08 20:54:23 +0530

    backend updated again

--- back-end/USERS_MOCK_DATA.js
+++ back-end/USERS_MOCK_DATA.js
@@ -8,26 +8,6 @@ const users = [
       school: "College of Arts & Science",
       major: "Computer Science",
       bio: "I am an NYU student and a good data and software researcher. I believe in the great power of technology."
-    },
-    {
-      id: 2,
-      dp: "https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg.jpg",
-      name: "Elvira Fitzgerald",
-      year: "Freshman",
-      university: "New York University",
-      school: "Stern School of Business",
-      major: "Business",
-      bio: "I am an NYU student and a good data and software researcher. I believe in the great power of technology."
-    },
-    {
-      id: 3,
-      dp: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFsZSUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80",
-      name: "Nelson Collier",
-      year: "Senior",
-      university: "New York University",
-      school: "Tisch School of Arts",
-      major: "Creative Arts",
-      bio: "I am an NYU student and a good data and software researcher. I believe in the great power of technology."
     }
 ];
 
--- front-end/src/routes/profile-page/profile.jsx
+++ front-end/src/routes/profile-page/profile.jsx
@@ -1,31 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './profile.css';
 
 import mock_user from './../../MOCK_DATA_USERS.json';
 
 function Profile() {
+  const [data, setData] = useState({})
+  useEffect(()=>{
+    fetch("http://localhost:4000/api/users")
+    .then(res => res.json())
+    .then(data => setData(data))
+  }, [])
     return (
         <>
         <h1 className="heading">PROFILE</h1>
             <aside className="profile-card">
   <header>
     {/* here’s the avatar */}
-      <img className="dp" src={mock_user[0].dp} />
+      <img className="dp" src={data.dp} />
     <br></br>
     <br></br>
     {/* name */}
-    <h1 className="name">{mock_user[0].name}</h1>
+    <h1 className="name">{data.name}</h1>
     {/* and role or location */}
-    <h2 className="subtitle">{mock_user[0].year}</h2>
+    <h2 className="subtitle">{data.year}</h2>
     <br></br>
     
   </header>
   {/* bit of a bio; who are you? */}
   <div className="profile-bio">
-    <p className="school"><span className="heading-school">University:</span> {mock_user[0].university}</p>
-    <p className="school"><span className="heading-school">School:</span> {mock_user[0].school}</p>
-    <p className="major"><span className="heading-major">Major:</span> {mock_user[0].major}</p>
-    <p className="bio"><span className="heading-bio">Bio:</span> {mock_user[0].bio}</p>
+    <p className="school"><span className="heading-school">University:</span> {data.university}</p>
+    <p className="school"><span className="heading-school">School:</span> {data.school}</p>
+    <p className="major"><span className="heading-major">Major:</span> {data.major}</p>
+    <p className="bio"><span className="heading-bio">Bio:</span> {data.bio}</p>
   </div>
 </aside>
 <br></br>
