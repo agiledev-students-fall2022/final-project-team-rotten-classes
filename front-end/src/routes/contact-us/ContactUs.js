f1a60add9359882adb5495220dd63a46fbef0141
luoashley <97135228+luoashley@users.noreply.github.com>
2022-11-10 04:48:33 -0500

    change the contact us style to be responsive

--- front-end/src/routes/contact-us/ContactUs.js
+++ front-end/src/routes/contact-us/ContactUs.js
@@ -14,46 +14,31 @@ const ContactUs = () => {
         console.log(inputs);
         setSubmitting(true);
         setTimeout(() => {
-            setSubmitting(false);}, 3000)
+            setSubmitting(false);
+            }, 3000)
     }
     return (
         <div className="whole">
-            <h1 className="head">Get in touch!  </h1>
+            <h1 className="head">Get in touch!</h1>
             {submitting &&
-                <div className="submit">Submtting Form...</div>
+                <div className="submit">Submitting Form...</div>
             }
-            <div className="form">
-                <form onSubmit={handleSubmit}>
-                    <br />
-                    <br />
-                    <br />
-                    <label className="inputs">Name
-                        <br />
-                        <input type="text" name="name" value={inputs.name || ""} onChange={handleChange}/>
-                    </label>
-                    <br />
-                    <br />
-                    <br />
-                    <label className="inputs">Email
-                        <br />
-                        <input type="text" name="email" value={inputs.email || ""} onChange={handleChange}/>
-                    </label>
-                    <br />
-                    <br />
-                    <br />
-                    <label className="inputs">Message
-                        <br />
-                        <textarea type="Message" name="message" value={inputs.message || ""} onChange={handleChange}/></label>
-                    <br />
-                    <br />
-                    <br />
+            <div>
+                <form onSubmit={handleSubmit} className="contact-us-form">
+                    <input type="text" name="name" placeholder="Name" value={inputs.name || ""}
+                           className="feedback-input" onChange={handleChange}/>
+                    <input type="text" name="email" placeholder="Email" value={inputs.email || ""}
+                           className="feedback-input" onChange={handleChange}/>
+                    <textarea name="message" placeholder="Enter your feedback" value={inputs.message || ""}
+                              className="feedback-input-textarea"
+                              onChange={handleChange}/>
                     <input type="submit" value="Submit" />
                 </form>
-                <div className="contacts">
-                    <p>7 NE. Newcastle St.Brooklyn, NY 11211</p>
-                    <p>716-948-3100</p>
-                    <p>support@classcritic.io</p>
-                </div>
+            </div>
+            <div className="contacts">
+                <p>7 NE. Newcastle St.Brooklyn, NY 11211</p>
+                <p>716-948-3100</p>
+                <p>support@classcritic.io</p>
             </div>
         </div>
     )}
