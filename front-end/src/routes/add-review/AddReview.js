57e17376575d42cb5f9b98340332d8265b6a86a8
luoashley <97135228+luoashley@users.noreply.github.com>
2022-11-10 04:19:19 -0500

    change the addreview styles to be responsive

--- front-end/src/routes/add-review/AddReview.js
+++ front-end/src/routes/add-review/AddReview.js
@@ -14,36 +14,42 @@ function AddReview(){
         <input name="professor" type="text" className="feedback-input" placeholder="Professor"/>
         <input name="semester" type="text" className="feedback-input" placeholder="Semester"/>
         <h6>Rate the Class</h6>
-        <input type="radio" id="contactChoice1" name="contact" defaultValue="email"/>
-        <label htmlFor="contactChoice1">Awful</label>
-        <input type="radio" id="contactChoice2" name="contact" defaultValue="phone"/>
-        <label htmlFor="contactChoice2">OK</label>
-        <input type="radio" id="contactChoice3" name="contact" defaultValue="mail"/>
-        <label htmlFor="contactChoice3">Good</label>
-        <input type="radio" id="contactChoice3" name="contact" defaultValue="mail"/>
-        <label htmlFor="contactChoice3">Awesome</label>
+        <div>
+          <input type="radio" id="contactChoice1" name="contact" defaultValue="email"/>
+          <label htmlFor="contactChoice1">Awful</label>
+          <input type="radio" id="contactChoice2" name="contact" defaultValue="phone"/>
+          <label htmlFor="contactChoice2">OK</label>
+          <input type="radio" id="contactChoice3" name="contact" defaultValue="mail"/>
+          <label htmlFor="contactChoice3">Good</label>
+          <input type="radio" id="contactChoice3" name="contact" defaultValue="mail"/>
+          <label htmlFor="contactChoice3">Awesome</label>
+        </div>
         <h6>Level of Difficulty</h6>
-        <input type="radio" id="contactChoice1" name="contact" defaultValue="email"/>
-        <label htmlFor="contactChoice1">1</label>
-        <input type="radio" id="contactChoice2" name="contact" defaultValue="phone"/>
-        <label htmlFor="contactChoice2">2</label>
-        <input type="radio" id="contactChoice3" name="contact" defaultValue="mail"/>
-        <label htmlFor="contactChoice3">3</label>
-        <input type="radio" id="contactChoice3" name="contact" defaultValue="mail"/>
-        <label htmlFor="contactChoice3">4</label>
-        <input type="radio" id="contactChoice3" name="contact" defaultValue="mail"/>
-        <label htmlFor="contactChoice3">5</label>
+        <div>
+          <input type="radio" id="contactChoice1" name="contact" defaultValue="email"/>
+          <label htmlFor="contactChoice1">1</label>
+          <input type="radio" id="contactChoice2" name="contact" defaultValue="phone"/>
+          <label htmlFor="contactChoice2">2</label>
+          <input type="radio" id="contactChoice3" name="contact" defaultValue="mail"/>
+          <label htmlFor="contactChoice3">3</label>
+          <input type="radio" id="contactChoice3" name="contact" defaultValue="mail"/>
+          <label htmlFor="contactChoice3">4</label>
+          <input type="radio" id="contactChoice3" name="contact" defaultValue="mail"/>
+          <label htmlFor="contactChoice3">5</label>
+        </div>
         <h6>Level of Workload</h6>
-        <input type="radio" id="contactChoice1" name="contact" defaultValue="email"/>
-        <label htmlFor="contactChoice1">1</label>
-        <input type="radio" id="contactChoice2" name="contact" defaultValue="phone"/>
-        <label htmlFor="contactChoice2">2</label>
-        <input type="radio" id="contactChoice3" name="contact" defaultValue="mail"/>
-        <label htmlFor="contactChoice3">3</label>
-        <input type="radio" id="contactChoice3" name="contact" defaultValue="mail"/>
-        <label htmlFor="contactChoice3">4</label>
-        <input type="radio" id="contactChoice3" name="contact" defaultValue="mail"/>
-        <label htmlFor="contactChoice3">5</label>
+        <div>
+          <input type="radio" id="contactChoice1" name="contact" defaultValue="email"/>
+          <label htmlFor="contactChoice1">1</label>
+          <input type="radio" id="contactChoice2" name="contact" defaultValue="phone"/>
+          <label htmlFor="contactChoice2">2</label>
+          <input type="radio" id="contactChoice3" name="contact" defaultValue="mail"/>
+          <label htmlFor="contactChoice3">3</label>
+          <input type="radio" id="contactChoice3" name="contact" defaultValue="mail"/>
+          <label htmlFor="contactChoice3">4</label>
+          <input type="radio" id="contactChoice3" name="contact" defaultValue="mail"/>
+          <label htmlFor="contactChoice3">5</label>
+        </div>
         <input name="title" type="text" className="feedback-input" placeholder="Title"/>
         <textarea name="text" className="feedback-input" placeholder="Comment" defaultValue={""}/>
         <input type="submit" defaultValue="SUBMIT" />
