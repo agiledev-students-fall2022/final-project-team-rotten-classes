0359ce1022903249b076abf64c77efa6f6785eb7
luoashley <97135228+luoashley@users.noreply.github.com>
2022-11-22 12:50:48 -0500

    Modify the signup component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,17 +4,15 @@ import './App.css';
 import Home from './routes/home/home.component';
 import Header from './Header';
 import Viewall from './Viewall';
-import CourseRatings from './components/course-ratings/CourseRatings';
-import CourseReviews from './components/course-reviews/CourseReviews';
 import CourseDetails from './components/course-details/CourseDetails';
 import Footer from './Footer';
 import BottomNavigationBar from './routes/bottom-navigation-bar/bottom-navigation-bar.component';
 import AddReview from './routes/add-review/AddReview';
 import SearchPage from "./routes/search-page/search-page.component";
 import ContactUs from "./routes/contact-us/ContactUs";
-import Login from './Login';
 import Profile from './routes/profile-page/profile';
 import Course from './components/course/course.component';
+import SignUp from "./routes/sign-up/sign-up.component";
 
 function App() {
   const[data, setData] = useState([])
@@ -35,12 +33,10 @@ function App() {
     <Header />
    <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<SignUp />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/Viewall" element={<Viewall />} />
         <Route path="/Course/:courseId" element={<Course />} />
-        {/* <Route path="/CourseRatings" element={<CourseRatings />} />
-        <Route path="/CourseReviews" element={<CourseReviews />} /> */}
         <Route path="/CourseDetails" element={<CourseDetails />} />
         <Route path="/AddReview" element={<AddReview />} />
         <Route path="/contact" element={<ContactUs />} />
