10df2458e62c7e2f8aae4783d7e89d398ee7d6a0
Sonia Jain <soniaj245@gmail.com>
2022-12-07 16:19:14 -0500

    testing

--- front-end/src/routes/home/home.component.jsx
+++ front-end/src/routes/home/home.component.jsx
@@ -20,7 +20,7 @@ function Home() {
       <br/>
       <br/>
       <div className="heading-highest-rated">
-        <h6 className="head1">HIGHEST RATED CLASSES <Badge bg="danger">ALL</Badge></h6>
+        <h6 className="head1">HIGHEST RATED CLASSESS <Badge bg="danger">ALL</Badge></h6>
         <h6 className="head2"><Button onClick={()=>{setView(true)}}>MORE</Button></h6>
       </div>
       <Row />
